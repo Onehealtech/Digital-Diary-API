1a9bb0f179384b49ea5b64c705f8a27fb2550727
nipen026 <khantnipen@gmail.com>
2026-03-03 14:58:56 +0530

    bug fix for complete task

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -141,9 +141,7 @@ class TaskController {
       const assistantId = req.user?.id;
       const role = req.user?.role;
 
-      if (!assistantId || role !== "ASSISTANT") {
-        return sendError(res, "Only assistants can complete tasks", 403);
-      }
+      
 
       const { notes } = req.body;
 
