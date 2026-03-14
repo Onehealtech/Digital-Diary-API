31058554c3fc22cefbd626b7975306cc93482c00
Yash Srivastava <119624888+yashhari@users.noreply.github.com>
2026-03-14 10:44:04 +0530

    updates

--- src/service/dashboard.service.ts
+++ src/service/dashboard.service.ts
@@ -481,7 +481,7 @@ class DashboardService {
   async getAllSuperAdmins() {
     const getSuperAdmins = await AppUser.findAll({
       where: { role: "SUPER_ADMIN" },
-      attributes: ["id", "fullName", "email","phone", "createdAt"],
+      attributes: ["id", "fullName", "email", "phone", "isActive", "createdAt"],
     });
     return getSuperAdmins;
   }
